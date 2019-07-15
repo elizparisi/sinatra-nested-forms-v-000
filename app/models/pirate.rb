02a669ca2fd542f118d1ac61525b98a7a80e6e42
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-07-15 02:03:36 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  
+  attr_accessor :name, :weight, :height 
+  
+  PIRATES = []
+  
+  def initialize(params)
+    @name = params[:name]
+    @weight = [:weight]
+    @height = params[:height]
+    PIRATES << self 
+  end
+  
+  def self.all 
+    PIRATES 
+  end
 end
